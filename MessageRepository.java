1c26a61157c55bf485303ae0924cc175e2fe79cf
Kartheek18153 <kartheek18153@gmail.com>
2025-09-28 10:19:26 +0530

    Delete MessageRepository.java

--- MessageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.chat.repository;
-
-import com.example.chat.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {}
