83f0cb5404eed64dde850f4d084c8a0259559ae2
Kartheek18153 <kartheek18153@gmail.com>
2025-09-28 10:32:03 +0530

    Delete src/main/java/com/example/chat/repository/BadWordRepository.java

--- src/main/java/com/example/chat/repository/BadWordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.chat.repository;
-
-import com.example.chat.model.BadWord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BadWordRepository extends JpaRepository<BadWord, Long> { }
