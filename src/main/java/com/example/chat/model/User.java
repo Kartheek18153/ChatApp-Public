30443f99af9180908a3a4be84d665edca4911b3e
Kartheek18153 <kartheek18153@gmail.com>
2025-09-29 11:34:05 +0530

    Update User.java

--- src/main/java/com/example/chat/model/User.java
+++ src/main/java/com/example/chat/model/User.java
@@ -15,11 +15,10 @@ public class User {
     @Column(nullable=false)
     private String password;
 
-    // Getters & Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public Long getId(){ return id; }
+    public void setId(Long id){ this.id = id; }
+    public String getUsername(){ return username; }
+    public void setUsername(String username){ this.username = username; }
+    public String getPassword(){ return password; }
+    public void setPassword(String password){ this.password = password; }
 }
