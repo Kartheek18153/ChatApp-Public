b6a74f587da680c84a898a8fd1d91a53f08dcd87
Kartheek18153 <kartheek18153@gmail.com>
2025-10-01 10:38:42 +0530

    Update MessageRepository.java

--- src/main/java/com/example/chat/repository/MessageRepository.java
+++ src/main/java/com/example/chat/repository/MessageRepository.java
@@ -3,4 +3,5 @@ package com.example.chat.repository;
 import com.example.chat.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {}
+public interface MessageRepository extends JpaRepository<Message, Long> {
+}
