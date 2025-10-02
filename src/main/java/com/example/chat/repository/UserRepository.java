192286f0c11f593d89910143ac0c49aba433f385
Kartheek18153 <kartheek18153@gmail.com>
2025-10-02 12:30:26 +0530

    Update UserRepository.java

--- src/main/java/com/example/chat/repository/UserRepository.java
+++ src/main/java/com/example/chat/repository/UserRepository.java
@@ -1,8 +1,9 @@
 
-package com.example.clario.repository;
+package com.example.chat.repository;
 
-import com.example.clario.model.User;
+import com.example.chat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
