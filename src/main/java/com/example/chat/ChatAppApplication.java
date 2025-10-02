ac5778651ab54f6a84f72ca656116339ba137bb2
Kartheek18153 <kartheek18153@gmail.com>
2025-10-02 12:31:36 +0530

    Update ChatAppApplication.java

--- src/main/java/com/example/chat/ChatAppApplication.java
+++ src/main/java/com/example/chat/ChatAppApplication.java
@@ -3,10 +3,8 @@ package com.example.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
-@EnableWebSocket
 public class ChatAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatAppApplication.class, args);
